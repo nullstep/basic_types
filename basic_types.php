dd95fa716c7b0396409f899194f571d40ac6a504
nullstep <me@nullstep.com>
2024-06-17 02:31:16 +0100

    version update

--- basic_types.php
+++ basic_types.php
@@ -6,7 +6,7 @@
  * Description: custom post/taxonomy/roles stuff
  * Author: nullstep
  * Author URI: https://nullstep.com
- * Version: 1.3.0
+ * Version: 1.3.1
 */
 
 defined('ABSPATH') or die('⎺\_(ツ)_/⎺');

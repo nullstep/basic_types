210ad20bf2c8ef8237419e5dc0a2de5897a55269
nullstep <me@nullstep.com>
2024-07-26 22:13:49 +0100

    buttons

--- basic_types.php
+++ basic_types.php
@@ -156,6 +156,8 @@ class BT {
 		if (is_admin()) {
 			self::init_menu();
 
+			/*
+
 			if (get_option('auth_key') !== '') {
 				$updater = new WPU(__FILE__);
 				$updater->set_versions('6.4', '6.4.3');
@@ -164,6 +166,8 @@ class BT {
 				$updater->authorize(get_option('auth_key'));
 				$updater->initialize();
 			}
+
+			*/
 		}
 
 		// register post types
@@ -774,6 +778,9 @@ class BT {
 		$idp = strtolower(self::$def['prefix']);
 ?>
 		<style>
+			#<?php echo $idp; ?>_meta_box {
+				position: relative;
+			}
 			#<?php echo $idp; ?>_meta_box .field-title {
 				position: absolute;
 				display: inline-block;
@@ -798,7 +805,6 @@ class BT {
 				box-sizing: border-box;
 				display: inline-block;
 				padding: 3px;
-				width: 100%;
 				vertical-align: middle;
 				margin-top: 10px;
 			}
@@ -949,6 +955,22 @@ class BT {
 				width: 73%;
 				padding: 3px;
 				margin-top: 10px;
+			}
+			#<?php echo $idp; ?>_meta_box .choose-colour-button {
+				margin-top: 9px;
+				height: 37px;
+			}
+			#<?php echo $idp; ?>_meta_box .choose-file-button {
+				position: relative;
+				top: 7px;
+				margin-right: 1px;
+				height: 37px;
+			}
+			#<?php echo $idp; ?>_meta_box .view-file-button {
+				position: relative;
+				top: 7px;
+				margin-left: 1px;
+				height: 37px;
 			}
 		</style>
 		<script>
@@ -1650,181 +1672,6 @@ class BT {
 	}
 }
 
-//  ███    █▄      ▄███████▄  ████████▄      ▄████████      ███         ▄████████  
-//  ███    ███    ███    ███  ███   ▀███    ███    ███  ▀█████████▄    ███    ███  
-//  ███    ███    ███    ███  ███    ███    ███    ███     ▀███▀▀██    ███    █▀   
-//  ███    ███    ███    ███  ███    ███    ███    ███      ███   ▀   ▄███▄▄▄      
-//  ███    ███  ▀█████████▀   ███    ███  ▀███████████      ███      ▀▀███▀▀▀      
-//  ███    ███    ███         ███    ███    ███    ███      ███        ███    █▄   
-//  ███    ███    ███         ███   ▄███    ███    ███      ███        ███    ███  
-//  ████████▀    ▄████▀       ████████▀     ███    █▀      ▄████▀      ██████████
-
-if (!class_exists('WPU')) {
-	class WPU {
-		private $file;
-		private $plugin;
-		private $basename;
-		private $active;
-		private $username;
-		private $repository;
-		private $authorize_token;
-		private $github_response;
-
-		private $requires;
-		private $tested;
-
-		public function __construct($file) {
-			$this->file = $file;
-			add_action('admin_init', [$this, 'set_plugin_properties']);
-
-			return $this;
-		}
-
-		public function set_plugin_properties() {
-			$this->plugin = get_plugin_data($this->file);
-			$this->basename = plugin_basename($this->file);
-			$this->active = is_plugin_active($this->basename);
-		}
-
-		public function set_versions($requires, $tested) {
-			$this->requires = $requires;
-			$this->tested = $tested;
-		}
-
-		public function set_username($username) {
-			$this->username = $username;
-		}
-
-		public function set_repository($repository) {
-			$this->repository = $repository;
-		}
-
-		public function authorize($token) {
-			$this->authorize_token = $token;
-		}
-
-		private function get_repository_info() {
-			if (is_null($this->github_response)) {
-				$request_uri = sprintf('https://api.github.com/repos/%s/%s/releases', $this->username, $this->repository);
-
-				$curl = curl_init();
-
-				curl_setopt_array($curl, [
-					CURLOPT_URL => $request_uri,
-					CURLOPT_RETURNTRANSFER => true,
-					CURLOPT_ENCODING => '',
-					CURLOPT_MAXREDIRS => 10,
-					CURLOPT_TIMEOUT => 0,
-					CURLOPT_FOLLOWLOCATION => true,
-					CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-					CURLOPT_CUSTOMREQUEST => 'GET',
-					CURLOPT_HTTPHEADER => [
-						'Authorization: token ' . $this->authorize_token,
-						'User-Agent: WPUpdater/1.0.0'
-					]
-				]);
-
-				$response = curl_exec($curl);
-
-				curl_close($curl);
-
-				$response = json_decode($response, true);
-
-				if (is_array($response)) {
-					$response = current($response);
-				}
-
-				$this->github_response = $response;
-			}
-		}
-
-		public function initialize() {
-			add_filter('pre_set_site_transient_update_plugins', [$this, 'modify_transient'], 10, 1);
-			add_filter('plugins_api', [$this, 'plugin_popup'], 10, 3);
-			add_filter('upgrader_post_install', [$this, 'after_install'], 10, 3);
-		}
-
-		public function modify_transient($transient) {
-			if (property_exists($transient, 'checked')) {
-				if ($checked = $transient->checked) {
-					$this->get_repository_info();
-
-					if (isset($this->github_response['tag_name'])) {
-						$out_of_date = version_compare($this->github_response['tag_name'], $checked[$this->basename], 'gt');
-					}
-					else {
-						$out_of_date = false;
-					}
-
-					if ($out_of_date) {
-						$new_files = $this->github_response['zipball_url'];
-						$slug = current(explode('/', $this->basename));
-
-						$plugin = [
-							'url' => $this->plugin['PluginURI'],
-							'slug' => $slug,
-							'package' => $new_files,
-							'new_version' => $this->github_response['tag_name']
-						];
-
-						$transient->response[$this->basename] = (object) $plugin;
-					}
-				}
-			}
-
-			return $transient;
-		}
-
-		public function plugin_popup($result, $action, $args) {
-			if ($action !== 'plugin_information') {
-				return false;
-			}
-
-			if (!empty($args->slug)) {
-				if ($args->slug == current(explode('/' , $this->basename))) {
-					$this->get_repository_info();
-
-					$plugin = [
-						'name' => $this->plugin['Name'],
-						'slug' => $this->basename,
-						'requires' => $this->$requires ?? '6.3',
-						'tested' => $this->$tested ?? '6.4.3',
-						'version' => $this->github_response['tag_name'],
-						'author' => $this->plugin['AuthorName'],
-						'author_profile' => $this->plugin['AuthorURI'],
-						'last_updated' => $this->github_response['published_at'],
-						'homepage' => $this->plugin['PluginURI'],
-						'short_description' => $this->plugin['Description'],
-						'sections' => [
-							'Description' => $this->plugin['Description'],
-							'Updates' => $this->github_response['body'],
-						],
-						'download_link' => $this->github_response['zipball_url']
-					];
-
-					return (object) $plugin;
-				}
-			}
-
-
-			return $result;
-		}
-
-		public function after_install($response, $hook_extra, $result) {
-			global $wp_filesystem;
-
-			$install_directory = plugin_dir_path($this->file);
-			$wp_filesystem->move($result['destination'], $install_directory);
-			$result['destination'] = $install_directory;
-
-			if ($this->active) {
-				activate_plugin($this->basename);
-			}
-
-			return $result;
-		}
-	}
-}
 
 //  ▀█████████▄    ▄██████▄    ▄██████▄       ███      
 //    ███    ███  ███    ███  ███    ███  ▀█████████▄  
